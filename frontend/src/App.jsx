8e12785d544bf7d5d41ed0d95ec93d8cfd8083be
rudan <ruddd8256@naver.com>
2022-04-07 23:50:30 +0900

    컴포넌트 구성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import axios from "axios";
-import RestAPI from "./RestAPI.jsx";
-
+//import RestAPI from "./RestAPI";
+import Header  from './components/Header';
+import Footer  from './components/Footer';
+import Main  from './components/Main';
+import MyNFT  from './components/MyNFT';
+import Create  from './components/Create';
+import MarketPlace  from './components/MarketPlace';
 
 function App() {
   return (
@@ -11,7 +15,16 @@ function App() {
     <div className="App">
        
       <header className="App-header">
-        <RestAPI />
+        <BrowserRouter>
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Main/>}></Route>
+            <Route path="/MyNFT" element={<MyNFT/>}></Route>
+            <Route path="/Create" element={<Create/>}></Route>
+            <Route path="/MarketPlace" element={< MarketPlace/>}></Route>
+          </Routes>
+          <Footer/>
+        </BrowserRouter>
       </header>
     </div>
   );
---
+++ frontend/src/components/Create.jsx
@@ -0,0 +1,9 @@
+
+function Create(){
+
+    return(
+        <p>엔프티 생성 dsadas</p>
+    );
+}
+
+export default Create
